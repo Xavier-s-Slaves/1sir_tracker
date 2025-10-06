6e4094f908537032522d8436d4a0987351e3bf5f
Ishneet Sukhvinder Singh <85265554+Ishneet0710@users.noreply.github.com>
2025-10-06 16:53:13 +0800

    add

--- app.py
+++ app.py
@@ -3940,11 +3940,11 @@ elif feature == "Analytics":
                 
                 # Define SBO 3 requirements (same as TAB 7)
                 sbo3_requirements = {
-                    "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl "], "current": 0},
+                    "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl ", "oregon circuit ", "oc ", "oregon "], "current": 0},
                     "Strength & Power": {"target": 12, "keywords": ["strength and power", "strength & power", "s&p", "s & p",], "current": 0},
                     "Interval Fast March": {"target": 3, "keywords": ["interval fast march", "ifm ", "route march"], "current": 0},
                     "Combat Circuit": {"target": 1, "keywords": ["combat circuit", "cc "], "current": 0},
-                    "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc "], "current": 0},
+                    "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc ", "oregon circuit ", "oc ", "oregon "], "current": 0},
                     "Sports & Games": {"target": 2, "keywords": ["sports and games", "sports & games", "s&g", "s & g",], "current": 0}
                 }
                 
@@ -4093,11 +4093,11 @@ elif feature == "Analytics":
             
             # Define SBO 3 requirements
             sbo3_requirements = {
-                "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl "], "current": 0},
+                "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl ", "oregon circuit ", "oc ", "oregon "], "current": 0},
                 "Strength & Power": {"target": 12, "keywords": ["strength and power", "strength & power", "s&p", "s & p",], "current": 0},
                 "Interval Fast March": {"target": 3, "keywords": ["interval fast march", "ifm ", "route march"], "current": 0},
                 "Combat Circuit": {"target": 1, "keywords": ["combat circuit", "cc "], "current": 0},
-                "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc "], "current": 0},
+                "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc ", "oregon circuit ", "oc ", "oregon "], "current": 0},
                 "Sports & Games": {"target": 2, "keywords": ["sports and games", "sports & games", "s&g", "s & g",], "current": 0}
             }
             
@@ -4617,11 +4617,11 @@ elif feature == "Analytics":
         
         # Define SBO 3 requirements (same as TAB 7)
         sbo3_requirements = {
-            "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl "], "current": 0},
+            "Cardio": {"target": 10, "keywords": ["distance interval", "endurance run", "fartlek", "di ", " er ", " fl ", "oregon circuit ", "oc ", "oregon "], "current": 0},
             "Strength & Power": {"target": 12, "keywords": ["strength and power", "strength & power", "s&p", "s & p",], "current": 0},
             "Interval Fast March": {"target": 3, "keywords": ["interval fast march", "ifm ", "route march"], "current": 0},
             "Combat Circuit": {"target": 1, "keywords": ["combat circuit", "cc "], "current": 0},
-            "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc "], "current": 0},
+            "Functional Training": {"target": 3, "keywords": ["functional training", "metabolic circuit", "ft ", "mc ", "oregon circuit ", "oc ", "oregon "], "current": 0},
             "Sports & Games": {"target": 2, "keywords": ["sports and games", "sports & games", "s&g", "s & g",], "current": 0}
         }
         
